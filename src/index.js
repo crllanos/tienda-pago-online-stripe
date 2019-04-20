42d23499d01208bd4d299754982791c795c35366
cristian llanos <cristian.llanos@gmail.com>
2019-04-20 11:12:37 -0400

    ADD: Init project. Product view, Stripe integration.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express   = require('express');
+const exphbs    = require('express-handlebars');
+const path      = require('path');
+const app       = express();
+
+
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'inc'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(require('./routes'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
